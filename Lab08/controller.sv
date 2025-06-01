ed4c9f9f95cce8fd9d105b9a3fe1d83872e388a3
Julio Nunes Avelar <jn5.guape@gmail.com>
2025-06-01 20:44:41 -0300

    Iniciando controlador

--- Lab08/controller.sv
+++ Lab08/controller.sv
@@ -1,11 +1,11 @@
 module Sdram_Ctrl #(
-    parameter DATA_WIDTH = 32,
-    parameter ADDR_WIDTH = 16,
-    parameter MEM_SIZE = 1024 * 1024 * 64, // 64MB
-    parameter SDRAM_CLK_FREQ = 100_000_000, // 100 MHz
-    parameter SDRAM_WORD_SIZE = 16, // 16 bits per word
-    parameter SDRAM_COL_WIDTH = 13, // Number of column
-    parameter SDRAM_ROW_WIDTH = 10, // Number of row
+    parameter DATA_WIDTH       = 32,
+    parameter ADDR_WIDTH       = 16,
+    parameter MEM_SIZE         = 1024 * 1024 * 64, // 64MB
+    parameter SDRAM_CLK_FREQ   = 100_000_000, // 100 MHz
+    parameter SDRAM_WORD_SIZE  = 16, // 16 bits per word
+    parameter SDRAM_COL_WIDTH  = 13, // Number of column
+    parameter SDRAM_ROW_WIDTH  = 10, // Number of row
     parameter SDRAM_BANK_WIDTH = 2, // Number of banks
     parameter SDRAM_ADDR_WIDTH = 13
 ) (
@@ -21,42 +21,147 @@ module Sdram_Ctrl #(
     output logic ack_o, // Acknowledge signal
 
     // SDRAM interface
-    inout  logic [15:0] dram_dq,
+    input  logic [15:0] dram_dq_in,
+    output logic [15:0] dram_dq_out,
     output logic [12:0] dram_addr,
-    output logic [1:0] dram_ba, // Bank address
-    output logic dram_clk, // Clock signal
-    output logic dram_cke, // Clock enable
-    output logic dram_ldqm, // Load mode register
-    output logic dram_udqm, // Data mask signals
-    output logic dram_we_n, // Write enable
-    output logic dram_cas_n, // Column address strobe
-    output logic dram_ras_n, // Row address strobe
-    output logic dram_cs_n // Chip select
+    output logic [1:0] dram_ba,    // Bank address
+    output logic dram_clk,         // Clock signal
+    output logic dram_cke,         // Clock enable
+    output logic dram_ldqm,        // Load mode register
+    output logic dram_udqm,        // Data mask signals
+    output logic dram_we_n,        // Write enable
+    output logic dram_cas_n,       // Column address strobe
+    output logic dram_ras_n,       // Row address strobe
+    output logic dram_cs_n,         // Chip select
+    output logic dram_dq_we
 );
 
 //SDRAM Commands [CS_N,RAS_N,CAS_N,WE_N]
-localparam DESL = 4'b1000;                                    //Device deselect
-localparam NOP = 4'b0111;                                     //No operation
-localparam BST = 4'b0110;                                     //Burst stop
-localparam RD = 4'b0101;                                      //For read with auto precharge A10 is '1' else '0'
-localparam WRT = 4'b0100;                                     //For write with auto precharge A10 is '1' else '0'
-localparam ACT = 4'b0011;                                     //Activate
-localparam PRE = 4'b0010;                                     //Precharge. To precharge all banks A10 is '1' else '0'
-localparam REF = 4'b0001;                                     //CBR auto-refrsh. For self-refresh toggle CKE along with CS_N
-localparam MRS = 4'b0000;                                     //Mode register set
+localparam DESL = 4'b1000; //Device deselect
+localparam NOP  = 4'b0111; //No operation
+localparam BST  = 4'b0110; //Burst stop
+localparam RD   = 4'b0101; //For read with auto precharge A10 is '1' else '0'
+localparam WRT  = 4'b0100; //For write with auto precharge A10 is '1' else '0'
+localparam ACT  = 4'b0011; //Activate
+localparam PRE  = 4'b0010; //Precharge. To precharge all banks A10 is '1' else '0'
+localparam REF  = 4'b0001; //CBR auto-refrsh. For self-refresh toggle CKE along with CS_N
+localparam MRS  = 4'b0000; //Mode register set
+
+localparam INITIALIZATION_NS     = 100_000;
+localparam CLK_PERIOD            = 7.52;
+localparam INITIALIZATION_CYCLES = INITIALIZATION_NS / CLK_PERIOD;
 
 typedef enum logic [3:0] { 
-    IDLE = 4'b0000,
-    INIT = 4'b0001,
-    PRECHARGE = 4'b0010,
-    WAIT = 4'b0011,
-    AUTO_REFRESH = 4'b0100,
-    MODE_REG_SET = 4'b0101,
-    ACTIVE = 4'b0111,
-    WRITE = 4'b1000,
-    READ = 4'b1001,
-    READ_DOUT = 4'b1010
+    POWER_DOWN,
+    INITIALIZATION,
+    WAIT_MEMORY,
+    PRECHARGE_INITIALIZATION,
+    AUTO_REFRESH_START,
+    AUTO_REFRESH_FINISH,
+    MODE_REGISTER_SET,
+    IDLE,
+    AUTO_REFLESH,
+    ACTIVATE,
+    PRECHARGE,
+    WRITE,
+    READ,
+    READ_WB
 } state_t;
 
+state_t current_state, state_after_wait;
+logic [15:0] time_counter;
+logic [3:0] command;
+
+logic auto_reflesh_peding;
+logic write_operation;
+
+localparam MODE_REGISTER_MODE = 16'b0000000000000000; // Example mode register value, adjust as needed
+
+always_ff @( posedge sys_clk ) begin
+    if(!sys_rst_n) begin
+        current_state    <= POWER_DOWN;
+        state_after_wait <= POWER_DOWN;
+        dram_dq_we       <= 0;
+        dram_addr        <= 0;
+    end else begin
+        case (current_state)
+            POWER_DOWN: current_state <= INITIALIZATION;
+            WAIT_MEMORY: begin
+                if(time_counter <= 1) begin
+                    current_state <= state_after_wait;
+                end else begin
+                    time_counter  <= time_counter - 1;
+                end
+            end
+            INITIALIZATION: begin
+                time_counter     <= INITIALIZATION_CYCLES;
+                current_state    <= WAIT_MEMORY;
+                state_after_wait <= PRECHARGE_INITIALIZATION;
+            end
+            PRECHARGE_INITIALIZATION: begin
+                time_counter     <= 2; // 15 ns PRE to ACT
+                current_state    <= WAIT_MEMORY;
+                state_after_wait <= AUTO_REFRESH_START;
+            end
+            AUTO_REFRESH_START: begin
+                time_counter     <= 8; // 60 ns REF to REF
+                current_state    <= WAIT_MEMORY;
+                state_after_wait <= AUTO_REFRESH_FINISH;
+            end
+            AUTO_REFRESH_FINISH: begin
+                time_counter     <= 8; // 60 ns REF to REF
+                current_state <= WAIT_MEMORY;
+                state_after_wait <= MODE_REGISTER_SET;
+            end
+            MODE_REGISTER_SET: begin
+                time_counter     <= 8; // 60 ns REF to REF
+                current_state  <= WAIT_MEMORY;
+                state_after_wait <= IDLE;
+            end
+            IDLE: begin
+                if(auto_reflesh_peding) begin
+                    current_state <= AUTO_REFLESH;
+                end else if(we_i || re_i) begin
+                    current_state <= ACTIVATE;
+                end else begin
+                    current_state <= IDLE;
+                end
+            end
+            AUTO_REFLESH: begin
+                time_counter     <= 8; // 60 ns REF to REF
+                current_state    <= WAIT_MEMORY;
+                state_after_wait <= IDLE;
+            end
+            ACTIVATE: begin
+                time_counter     <= 2; // 15 ns TRCD
+                current_state    <= IDLE;
+                state_after_wait <= (write_operation) ? WRITE : READ;
+            end
+
+            WRITE: begin
+                time_counter     <= 2; // 15 ns TRP
+                state_after_wait <= IDLE;
+                current_state    <= WAIT_MEMORY;
+            end
+
+            READ: begin
+                current_state <= READ_WB;
+            end
+
+            READ_WB: begin
+                time_counter     <= 2; // 15 ns TRP
+                current_state    <= WAIT_MEMORY;
+                state_after_wait <= IDLE;
+            end
+            default: begin
+                current_state <= POWER_DOWN;
+            end
+        endcase
+    end
+end
+
+
+assign {dram_cs_n, dram_ras_n, dram_cas_n, dram_we_n} = command;
+assign dram_clk   = sys_clk;
     
 endmodule
--- Lab08/controller_board.sv
+++ Lab08/controller_board.sv
@@ -7,7 +7,77 @@ module Controller_Board (
     output logic [0:6] HEX1,
     output logic [0:6] HEX2,
     output logic [0:6] HEX3,
-    output logic [0:6] HEX4
+    output logic [0:6] HEX4,
+
+    // SDRAM interface
+    inout  logic [15:0] DRAM_DQ,
+    output logic [12:0] DRAM_ADDR,
+    output logic [1:0]  DRAM_BA,    // Bank address
+    output logic        DRAM_CLK,   // Clock signal
+    output logic        DRAM_CKE,   // Clock enable
+    output logic        DRAM_LDQM,  // Load mode register
+    output logic        DRAM_UDQM,  // Data mask signals
+    output logic        DRAM_WE_N,  // Write enable
+    output logic        DRAM_CAS_N, // Column address strobe
+    output logic        DRAM_RAS_N, // Row address strobe
+    output logic        DRAM_CS_N   // Chip select
 );
+
+logic sys_clk;
+logic rst_n;
+logic dram_dq_we;
+logic [15:0] dram_dq_out, dram_dq_in;
+
+
+initial begin
+	rst_n = 1;
+end
+
+pll pll1 (
+    .refclk   (CLOCK_50), // refclk.clk
+    .rst      (~rst_n),   // reset.reset
+    .outclk_0 (sys_clk)   // outclk0.clk
+);
+
+Sdram_Ctrl #(
+    .DATA_WIDTH       (32),
+    .ADDR_WIDTH       (16),
+    .MEM_SIZE         (1024 * 1024 * 64), // 64MB
+    .SDRAM_CLK_FREQ   (133_000_000), // 100 MHz
+    .SDRAM_WORD_SIZE  (16), // 16 bits per word
+    .SDRAM_COL_WIDTH  (13),
+    .SDRAM_ROW_WIDTH  (10),
+    .SDRAM_BANK_WIDTH (2),
+    .SDRAM_ADDR_WIDTH (13)
+) u_sdram_ctrl (
+    .sys_clk    (sys_clk),
+    .sys_rst_n  (rst_n),
+
+    // Processor interface
+    .addr_i     (),
+    .data_i     (),
+    .we_i       (),
+    .re_i       (),
+    .data_o     (),
+    .ack_o      (),
+
+    // SDRAM interface
+    .dram_dq_in  (dram_dq_in),
+    .dram_dq_out (dram_dq_out),
+    .dram_addr   (DRAM_ADDR),
+    .dram_ba     (DRAM_BA),
+    .dram_clk    (DRAM_CLK),
+    .dram_cke    (DRAM_CKE),
+    .dram_ldqm   (DRAM_LDQM),
+    .dram_udqm   (DRAM_UDQM),
+    .dram_we_n   (DRAM_WE_N),
+    .dram_cas_n  (DRAM_CAS_N),
+    .dram_ras_n  (DRAM_RAS_N),
+    .dram_cs_n   (DRAM_CS_N),
+    .dram_dq_we  (dram_dq_we),
+);
+
+assign dram_dq_in = dram_dq;
+assign dram_dq    = (dram_dq_we) ? dram_dq_out : 16'bz;
     
 endmodule
