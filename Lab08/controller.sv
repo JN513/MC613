493c2d3df383f12cd0a773be75148cfedc18d326
Julio Nunes Avelar <jn5.guape@gmail.com>
2025-06-02 16:28:22 -0300

    Atualizando lab08

--- Lab08/controller.sv
+++ Lab08/controller.sv
@@ -1,11 +1,11 @@
 module Sdram_Ctrl #(
     parameter DATA_WIDTH       = 32,
-    parameter ADDR_WIDTH       = 16,
+    parameter ADDR_WIDTH       = 32,
     parameter MEM_SIZE         = 1024 * 1024 * 64, // 64MB
     parameter SDRAM_CLK_FREQ   = 100_000_000, // 100 MHz
     parameter SDRAM_WORD_SIZE  = 16, // 16 bits per word
-    parameter SDRAM_COL_WIDTH  = 13, // Number of column
-    parameter SDRAM_ROW_WIDTH  = 10, // Number of row
+    parameter SDRAM_COL_WIDTH  = 10, // Number of column
+    parameter SDRAM_ROW_WIDTH  = 13, // Number of row
     parameter SDRAM_BANK_WIDTH = 2, // Number of banks
     parameter SDRAM_ADDR_WIDTH = 13
 ) (
@@ -19,6 +19,7 @@ module Sdram_Ctrl #(
     input  logic re_i, // Read enable
     output logic [DATA_WIDTH-1:0] data_o,
     output logic ack_o, // Acknowledge signal
+    output logic busy_o, // Busy signal
 
     // SDRAM interface
     input  logic [15:0] dram_dq_in,
@@ -64,25 +65,26 @@ typedef enum logic [3:0] {
     ACTIVATE,
     PRECHARGE,
     WRITE,
+    WRITE_ACK,
     READ,
     READ_WB
 } state_t;
 
 state_t current_state, state_after_wait;
 logic [15:0] time_counter;
+logic [15:0] read_counter;
 logic [3:0] command;
+logic [1:0] latency;
 
 logic auto_reflesh_peding;
 logic write_operation;
 
-localparam MODE_REGISTER_MODE = 16'b0000000000000000; // Example mode register value, adjust as needed
+localparam MODE_REGISTER_MODE = 13'b0001000100000; //Latency=2, burst length=1, single access write operation
 
 always_ff @( posedge sys_clk ) begin
     if(!sys_rst_n) begin
         current_state    <= POWER_DOWN;
         state_after_wait <= POWER_DOWN;
-        dram_dq_we       <= 0;
-        dram_addr        <= 0;
     end else begin
         case (current_state)
             POWER_DOWN: current_state <= INITIALIZATION;
@@ -110,12 +112,12 @@ always_ff @( posedge sys_clk ) begin
             end
             AUTO_REFRESH_FINISH: begin
                 time_counter     <= 8; // 60 ns REF to REF
-                current_state <= WAIT_MEMORY;
+                current_state    <= WAIT_MEMORY;
                 state_after_wait <= MODE_REGISTER_SET;
             end
             MODE_REGISTER_SET: begin
                 time_counter     <= 8; // 60 ns REF to REF
-                current_state  <= WAIT_MEMORY;
+                current_state    <= WAIT_MEMORY;
                 state_after_wait <= IDLE;
             end
             IDLE: begin
@@ -140,18 +142,30 @@ always_ff @( posedge sys_clk ) begin
 
             WRITE: begin
                 time_counter     <= 2; // 15 ns TRP
-                state_after_wait <= IDLE;
+                state_after_wait <= WRITE_ACK;
                 current_state    <= WAIT_MEMORY;
             end
 
+            WRITE_ACK: begin
+                time_counter     <= 0;
+                current_state    <= IDLE;
+            end
+
             READ: begin
+                read_counter  <= 0; // Reset read counter
+                time_counter  <= 0; // Latency for read operation
                 current_state <= READ_WB;
             end
 
             READ_WB: begin
                 time_counter     <= 2; // 15 ns TRP
-                current_state    <= WAIT_MEMORY;
-                state_after_wait <= IDLE;
+                if(read_counter < latency - 1) begin
+                    read_counter <= read_counter + 1; // Increment read counter
+                    current_state <= READ_WB; // Stay in READ_WB state until latency is met
+                end else begin
+                    current_state    <= WAIT_MEMORY;
+                    state_after_wait <= IDLE; // After read, go back to IDLE
+                end
             end
             default: begin
                 current_state <= POWER_DOWN;
@@ -160,6 +174,140 @@ always_ff @( posedge sys_clk ) begin
     end
 end
 
+
+always_ff @(posedge sys_clk) begin
+    if(!sys_rst_n) begin
+        busy_o      <= 0;
+        ack_o       <= 0;
+        command     <= DESL; // Device deselect
+        dram_dq_out <= 0;
+        dram_addr   <= 0;
+        dram_ba     <= 0;
+        dram_cke    <= 0; // Clock enable low
+        dram_ldqm   <= 0; // Load mode register low
+        dram_udqm   <= 0; // Data mask signals low
+        dram_dq_we  <= 0; // Data write enable low
+    end else begin
+        case (current_state)
+            POWER_DOWN: begin
+                command    <= DESL; // Device deselect
+                dram_cke   <= 0; // Clock enable low
+                dram_ldqm  <= 0; // Load mode register low
+                dram_udqm  <= 0; // Data mask signals low
+                busy_o     <= 0;
+                dram_dq_we <= 0; // Data write enable low
+            end
+            INITIALIZATION: begin
+                dram_cke   <= 1; // Clock enable high
+                dram_ldqm  <= 1; // Load mode register low
+                dram_udqm  <= 1; // Data mask signals low
+            end
+            WAIT_MEMORY: begin
+
+            end
+
+            PRECHARGE_INITIALIZATION: begin
+                command        <= PRE; // Precharge all banks
+                dram_addr[10]  <= 1; // Address is not used in this command
+            end
+
+            AUTO_REFRESH_START: begin
+                command <= REF; // CBR auto-refresh
+            end
+            AUTO_REFRESH_FINISH: begin
+                command <= REF; // No operation
+            end
+
+            MODE_REGISTER_SET: begin
+                command         <= MRS; // Mode register set
+                dram_addr[12:0] <= MODE_REGISTER_MODE[12:0]; // Set mode register value
+                dram_ba         <= 2'b00;
+                busy_o          <= 1; // Busy during mode register set
+
+                latency <= (MODE_REGISTER_MODE[6:4]) ? 2'b10 : 2'b11;
+            end
+
+            IDLE: begin
+                command    <= NOP; // No operation
+                busy_o     <= 0;   // Not busy
+                dram_dq_we <= 0;   // Data write enable low
+                dram_ldqm  <= 0;   // Load mode register low
+                dram_udqm  <= 0;   // Data mask signals low
+            end
+
+            AUTO_REFLESH: begin
+                command <= REF; // CBR auto-refresh
+                busy_o  <= 1; // Busy during auto-refresh
+            end
+
+            ACTIVATE: begin
+                command         <= ACT; // Activate command
+                dram_addr[12:0] <= addr_i[22:10]; // Set row address
+                dram_ba         <= addr_i[24:23]; // Set bank address
+                busy_o          <= 1; // Busy during activation
+            end
+
+            WRITE: begin
+                command        <= WRT; // Write command
+                dram_addr[9:0] <= addr_i[9:0]; // Set cow address
+                dram_addr[10]  <= 1; // Auto precharge
+                dram_dq_we     <= 1; // Data write enable high
+                dram_dq_out    <= data_i[15:0]; // Output data to SDRAM
+                dram_ldqm      <= 0; // Load mode register low
+                dram_udqm      <= 0; // Data mask signals low
+            end
+
+            WRITE_ACK: begin
+                ack_o      <= 1; // Acknowledge write operation
+                busy_o     <= 0; // Not busy after write
+                dram_dq_we <= 0; // Data write enable low
+            end
+
+            READ: begin
+                command        <= RD; // Read command
+                dram_addr[9:0] <= addr_i[9:0]; // Set cow address
+                dram_addr[10]  <= 1; // Auto precharge
+                dram_ldqm      <= 0; // Load mode register low
+                dram_udqm      <= 0; // Data mask signals low
+            end
+
+            READ_WB: begin
+                if(read_counter >= latency - 1) begin
+                    ack_o      <= 1; // Acknowledge read operation
+                    busy_o     <= 0; // Not busy after read
+                end
+                dram_dq_we <= 0; // Data write enable low
+                data_o     <= dram_dq_in; // Read data from SDRAM
+            end
+
+        endcase
+    end
+end
+
+
+// Auto refresh
+
+logic [9:0] refresh_counter;
+logic auto_reflesh_en;
+
+always_ff @( posedge sys_clk ) begin
+    if(!sys_rst_n) begin
+        refresh_counter <= 0;
+        auto_reflesh_peding <= 0;
+        auto_reflesh_en <= 0;
+    end begin
+        if(refresh_counter < 782 && auto_reflesh_en) begin
+            auto_reflesh_peding <= 0;
+            refresh_counter <= refresh_counter + 1;
+        end else if(auto_reflesh_en) begin
+            auto_reflesh_peding <= 1;
+            if(!busy_o)
+                refresh_counter <= 0;
+        end else if(current_state == IDLE && !auto_reflesh_en) begin
+            auto_reflesh_en <= 1'b1;
+        end
+    end
+end
 
 assign {dram_cs_n, dram_ras_n, dram_cas_n, dram_we_n} = command;
 assign dram_clk   = sys_clk;
--- Lab08/controller_board.sv
+++ Lab08/controller_board.sv
@@ -8,6 +8,7 @@ module Controller_Board (
     output logic [0:6] HEX2,
     output logic [0:6] HEX3,
     output logic [0:6] HEX4,
+    output logic [0:6] HEX5,
 
     // SDRAM interface
     inout  logic [15:0] DRAM_DQ,
@@ -31,7 +32,130 @@ logic [15:0] dram_dq_out, dram_dq_in;
 
 initial begin
 	rst_n = 1;
+    write = 0;
+    read  = 0;
 end
+
+
+logic [31:0] write_data;
+logic [31:0] read_data;
+logic [31:0] addr;
+logic write, read, ack, busy, pass, fail;
+
+logic [31:0] counter;
+
+logic w1, r1, rd;
+
+assign LEDR = {pass, 2'b10,w1, 1'b0,r1,1'b0,rd, 1'b0 ,fail};
+
+typedef enum logic [2:0] { 
+    INIT,
+    DELAY,
+    WRITE,
+    DELAY_2,
+    READ,
+    READ_WB,
+    COMPAIR
+} state_t;
+
+state_t state;
+
+logic [1:0] stt;
+
+initial begin
+    state = INIT;
+    stt = 0;
+end
+
+always_ff @( posedge CLOCK_50 ) begin
+    case (stt)
+        0: begin
+            rst_n <= 0;
+            stt <= 1;
+        end
+        1: begin
+            rst_n <= 0;
+            stt <= 2;
+        end 
+        2: begin
+            rst_n <= 1;
+        end
+    endcase
+end
+
+always_ff @( posedge sys_clk ) begin
+    read <= 0;
+    write <= 0;
+
+    if(!rst_n) begin
+        state <= INIT;
+    end else begin
+        case (state)
+            INIT: begin
+                w1 <=0;
+                r1 <= 0;
+                rd <= 0;
+                pass <= 0;
+                fail <= 0;
+                addr  <= 0;
+                write_data <= 32'h0000ABCD;
+                state <= DELAY;
+            end
+
+            DELAY: begin
+                if(counter < 1_000_000) begin
+                    counter <= counter + 1;
+                end else begin
+                    counter <= 0;
+                    state <= WRITE;
+                end
+            end
+
+            WRITE: begin
+                w1 <= 1;
+                if(!busy) begin
+                    write <= 1;
+                    state <= DELAY_2;
+                end
+            end
+
+            DELAY_2: begin
+                if(counter < 10) begin
+                    counter <= counter + 1;
+                end else begin
+                    counter <= 0;
+                    state <= READ;
+                end
+            end
+
+            READ: begin
+                r1 <= 0;
+                if(!busy) begin
+                    read <= 1;
+                    state <= READ_WB;
+                end
+            end
+
+            READ_WB: begin
+                if(ack) begin
+                    state <= COMPAIR;
+                end
+            end
+
+            COMPAIR: begin
+                rd <= 0;
+                if(read_data == write_data) begin
+                    pass <= 1;
+                    fail <= 0;
+                end else begin
+                    pass <= 0;
+                    fail <= 1;
+                end
+            end 
+        endcase
+    end
+end
+
 
 pll pll1 (
     .refclk   (CLOCK_50), // refclk.clk
@@ -41,7 +165,7 @@ pll pll1 (
 
 Sdram_Ctrl #(
     .DATA_WIDTH       (32),
-    .ADDR_WIDTH       (16),
+    .ADDR_WIDTH       (32),
     .MEM_SIZE         (1024 * 1024 * 64), // 64MB
     .SDRAM_CLK_FREQ   (133_000_000), // 100 MHz
     .SDRAM_WORD_SIZE  (16), // 16 bits per word
@@ -50,16 +174,17 @@ Sdram_Ctrl #(
     .SDRAM_BANK_WIDTH (2),
     .SDRAM_ADDR_WIDTH (13)
 ) u_sdram_ctrl (
-    .sys_clk    (sys_clk),
-    .sys_rst_n  (rst_n),
+    .sys_clk     (sys_clk),
+    .sys_rst_n   (rst_n),
 
     // Processor interface
-    .addr_i     (),
-    .data_i     (),
-    .we_i       (),
-    .re_i       (),
-    .data_o     (),
-    .ack_o      (),
+    .addr_i      (addr),
+    .data_i      (write_data),
+    .we_i        (write),
+    .re_i        (read),
+    .data_o      (read_data),
+    .ack_o       (ack),
+    .busy_o      (busy),
 
     // SDRAM interface
     .dram_dq_in  (dram_dq_in),
@@ -77,7 +202,7 @@ Sdram_Ctrl #(
     .dram_dq_we  (dram_dq_we),
 );
 
-assign dram_dq_in = dram_dq;
-assign dram_dq    = (dram_dq_we) ? dram_dq_out : 16'bz;
+assign dram_dq_in = DRAM_DQ;
+assign DRAM_DQ    = (dram_dq_we) ? dram_dq_out : 16'bz;
     
 endmodule
